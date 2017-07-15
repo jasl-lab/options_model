4ff99a55da276f5aa0f083c1904ac7bb6d540ad1
jasl <jasl9187@hotmail.com>
2017-07-15 16:50:40 +0800

    refactor

--- lib/options_model/concerns/attribute_assignment.rb
+++ lib/options_model/concerns/attribute_assignment.rb
@@ -1,77 +1,79 @@
-module OptionsModel::Concerns
-  module AttributeAssignment
-    extend ActiveSupport::Concern
+module OptionsModel
+  module Concerns
+    module AttributeAssignment
+      extend ActiveSupport::Concern
 
-    def initialize(attributes = {})
-      update_attributes(attributes)
-    end
-
-    def initialize_dup(other)
-      super
-
-      update_attributes(other)
-    end
-
-    def update_attributes(other)
-      return unless other
-
-      unless other.respond_to?(:to_h)
-        raise ArgumentError, "#{other} must be respond to `to_h`"
+      def initialize(attributes = {})
+        update_attributes(attributes)
       end
 
-      other.to_h.each do |k, v|
-        if respond_to?("#{k}=")
-          public_send("#{k}=", v)
-        else
-          unused_attributes[k] = v
+      def initialize_dup(other)
+        super
+
+        update_attributes(other)
+      end
+
+      def update_attributes(other)
+        return unless other
+
+        unless other.respond_to?(:to_h)
+          raise ArgumentError, "#{other} must be respond to `to_h`"
+        end
+
+        other.to_h.each do |k, v|
+          if respond_to?("#{k}=")
+            public_send("#{k}=", v)
+          else
+            unused_attributes[k] = v
+          end
         end
       end
-    end
 
-    def [](key)
-      public_send(key) if respond_to?(key)
-    end
-
-    def []=(key, val)
-      setter = "#{key}="
-      if respond_to?(setter)
-        public_send(setter, val)
-      else
-        unused_attributes[key] = val
-      end
-    end
-
-    def fetch(key, default = nil)
-      if self.class.attribute_names.exclude?(key.to_sym) && default.nil? && !block_given?
-        raise KeyError, "attribute not found"
+      def [](key)
+        public_send(key) if respond_to?(key)
       end
 
-      value = respond_to?(key) ? public_send(key) : nil
-      return value if value
-
-      if default
-        default
-      elsif block_given?
-        yield
+      def []=(key, val)
+        setter = "#{key}="
+        if respond_to?(setter)
+          public_send(setter, val)
+        else
+          unused_attributes[key] = val
+        end
       end
-    end
 
-    def _attributes
-      @attributes ||= ActiveSupport::HashWithIndifferentAccess.new
-    end
-    private :_attributes
-    alias_method :attributes, :_attributes
+      def fetch(key, default = nil)
+        if self.class.attribute_names.exclude?(key.to_sym) && default.nil? && !block_given?
+          raise KeyError, "attribute not found"
+        end
 
-    def _nested_attributes
-      @nested_attributes ||= ActiveSupport::HashWithIndifferentAccess.new
-    end
-    private :_nested_attributes
-    alias_method :nested_attributes, :_nested_attributes
+        value = respond_to?(key) ? public_send(key) : nil
+        return value if value
 
-    def _unused_attributes
-      @unused_attributes ||= ActiveSupport::HashWithIndifferentAccess.new
+        if default
+          default
+        elsif block_given?
+          yield
+        end
+      end
+
+      def _attributes
+        @attributes ||= ActiveSupport::HashWithIndifferentAccess.new
+      end
+      private :_attributes
+      alias_method :attributes, :_attributes
+
+      def _nested_attributes
+        @nested_attributes ||= ActiveSupport::HashWithIndifferentAccess.new
+      end
+      private :_nested_attributes
+      alias_method :nested_attributes, :_nested_attributes
+
+      def _unused_attributes
+        @unused_attributes ||= ActiveSupport::HashWithIndifferentAccess.new
+      end
+      private :_unused_attributes
+      alias_method :unused_attributes, :_unused_attributes
     end
-    private :_unused_attributes
-    alias_method :unused_attributes, :_unused_attributes
   end
 end
--- lib/options_model/concerns/name_hacking.rb
+++ lib/options_model/concerns/name_hacking.rb
@@ -1,18 +1,20 @@
-module OptionsModel::Concerns
-  module NameHacking
-    extend ActiveSupport::Concern
+module OptionsModel
+  module Concerns
+    module NameHacking
+      extend ActiveSupport::Concern
 
-    module ClassMethods
-      def name
-        @_name
-      end
-
-      def name=(value)
-        unless /^[A-Z][a-zA-Z_0-9]*$/ =~ value
-          raise ArgumentError, "`name` must a valid class name"
+      module ClassMethods
+        def name
+          @_name
         end
 
-        @_name = value
+        def name=(value)
+          unless /^[A-Z][a-zA-Z_0-9]*$/ =~ value
+            raise ArgumentError, "`name` must a valid class name"
+          end
+
+          @_name = value
+        end
       end
     end
   end
